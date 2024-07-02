fd8d345d82891313bc2e6f414a8351874cdca8a2
Tancd <caotan1008@gmail.com>
2024-07-03 00:24:09 +0900

    .

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://koriya-be.onrender.com"
+    // const url = "https://koriya-be.onrender.com"
+    const url = "http://localhost:4000"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
