b36fab6f1e0794a1869aae2e5175d290f0156f0b
Tancd <caotan1008@gmail.com>
2024-07-04 00:27:40 +0900

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: '/',
 })
